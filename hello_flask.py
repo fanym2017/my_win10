09d1bf7b3e7ed8ae79740ba3309f6ef38a31b154
fanym2017 <55376415+fanym2017@users.noreply.github.com>
2020-08-13 09:39:14 +0800

    flask测试

---
+++ hello_flask.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "index"
+
+@app.route('/hello/<name>')
+def hello(name):
+    print(name)
+    return render_template('index.html', name=name)
+
+app.run()
